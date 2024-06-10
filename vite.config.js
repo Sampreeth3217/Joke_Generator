1f3aa6db2e4e118d4f6b47313762cfb49e5a3708
Sampreeth3217 <nschowdary1234@gmail.com>
2024-06-10 09:42:11 +0530

    Deploying

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Get the repository name dynamically
+const repoName = 'Joke_Generator';
+
 export default defineConfig({
   plugins: [react()],
-  base:"/vite-deploy"
+  base: `/${repoName}/`
 });
